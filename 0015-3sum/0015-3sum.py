2795c7ad89285dcc3ba77d2977de6acb78eabdbd
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-03-12 01:36:04 +0100

    Time: 605 ms (42.72%), Space: 20.6 MB (84.6%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -9,13 +9,13 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:  
                 continue
             
-            left, right = i + 1, len(nums) - 1  # Two pointers
+            left, right = i + 1, len(nums) - 1 
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
                 if total == 0:
                     result.append([nums[i], nums[left], nums[right]])
                     
-                    # Skip duplicate values
+                   
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
