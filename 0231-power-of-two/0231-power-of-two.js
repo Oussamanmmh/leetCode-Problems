eb7bcfb5ec28bf50c7d2aaa86079c9a0e406d816
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-03-20 15:44:59 +0100

    Time: 0 ms (100%), Space: 57.2 MB (22.21%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    i = 0
+    if(n<0)return false
+    while(n !=0){
+        n=n&(n-1)
+        i++
+    }
+    return i===1 ? true : false 
+};
