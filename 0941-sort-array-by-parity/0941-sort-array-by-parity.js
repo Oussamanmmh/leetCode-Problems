c2e02a3c8b30509c90b1bf8673053978883229a1
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-07-31 09:34:31 +0200

    Time: 3 ms (51.18%), Space: 56 MB (97.65%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.js
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -3,18 +3,15 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-
-    const evens = [];
-    const odds = [];
-
-    for (let num of nums) {
-        if (num % 2 === 0) {
-            evens.push(num);
-        } else {
-            odds.push(num);
-        }
+   const tempNums = []
+    for(let i =0 ; i < nums.length  ; i++){
+            if(nums[i]%2!==0){
+               tempNums.push(nums[i])
+            }
+            else{
+                tempNums.unshift(nums[i])
+            }
     }
-
-    return [...evens, ...odds];
-   
+    
+   return tempNums ;
 };
