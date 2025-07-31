d7ad56ca65572d66eaf75d8d6fc3d16d2e94f4c4
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-07-31 07:16:58 +0200

    Time: 8 ms (31.1%), Space: 55.3 MB (65.41%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,16 +4,12 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    
-    let sumS = 0, sumT = 0;
-
-  for (let char of s) {
-    sumS += char.charCodeAt(0);
-  }
-
-  for (let char of t) {
-    sumT += char.charCodeAt(0);
-  }
-
-  return String.fromCharCode(sumT - sumS);
+    const count = {}
+    for(let c of s ){
+        count[c] = (count[c] || 0)  + 1 ;
+    }
+    for(let c of t){
+        if(!count[c]) return c
+        count[c]-- ;
+    }
 };
