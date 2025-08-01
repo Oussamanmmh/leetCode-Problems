9b1eba3611ee3b93a2ff39c62524a92f9b1a2591
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-08-01 23:16:35 +0200

    Time: 0 ms (100%), Space: 63.7 MB (87.32%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    return arr.indexOf(Math.max(...arr))
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] < arr[mid + 1]) {
+           
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;
 };
