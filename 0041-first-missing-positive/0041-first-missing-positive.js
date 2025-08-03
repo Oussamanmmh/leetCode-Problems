08dbc045719f4814a110a670a99ef6df1d8f7ef3
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-08-03 08:50:19 +0200

    Time: 1 ms (97.26%), Space: 62.9 MB (71.28%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,13 +3,25 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    let missNumber = 1 ;
-    nums.sort((a, b) => a - b) ;
-    console.log(nums);
-    for(let i=0; i< nums.length;i++){
-        if(missNumber === nums[i]){
-            missNumber ++ ;
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        while (
+            nums[i] > 0 &&
+            nums[i] <= n &&
+            nums[nums[i] - 1] !== nums[i]
+        ) {
+            let temp = nums[i];
+            nums[i] = nums[temp - 1];
+            nums[temp - 1] = temp;
         }
     }
-    return missNumber ;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
 };
