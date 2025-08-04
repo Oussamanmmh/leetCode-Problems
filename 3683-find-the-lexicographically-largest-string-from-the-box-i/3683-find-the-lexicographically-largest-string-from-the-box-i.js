b1e9697accf30e719f33c43e62f3a0ae9b05eb65
OUSSAMA NEMAMCHA <147724306+Oussamanmmh@users.noreply.github.com>
2025-08-04 07:41:48 +0200

    Time: 6 ms (28.57%), Space: 56 MB (78.27%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.js
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.js
@@ -10,7 +10,7 @@ var answerString = function(word, numFriends) {
     let n = word.length;
     let res = "";
     for (let i = 0; i < n; i++) {
-        let s = word.substring(i, i+n-numFriends+1);
+        let s = word.substring(i,i+n-numFriends+1);
         if (s > res) {
             res = s;
         }
